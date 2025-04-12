2f39a28bd4b3f46e6453bbb04cafcaa36544a051
mayankav <mayankmacav@gmail.com>
2025-04-13 00:09:40 +0530

    fix: removed console error

--- app/api/recordings/index.ts
+++ app/api/recordings/index.ts
@@ -27,7 +27,6 @@ export async function getLatestRecording(
     const response = await axiosClient.get("/recordings/latest", { params });
     return response.data;
   } catch (error) {
-    console.error("Error getting latest recording:", error);
     throw error;
   }
 }
@@ -47,7 +46,6 @@ export async function getAllRecordings(
     const response = await axiosClient.get("/recordings", { params });
     return response.data;
   } catch (error) {
-    console.error("Error getting all recordings:", error);
     throw error;
   }
 }
