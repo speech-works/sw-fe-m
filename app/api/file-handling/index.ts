f93d74b365f89b61605f261ed831e8ef1485e908
mayankav <mayankmacav@gmail.com>
2026-03-04 06:21:04 +0530

    feat: Add logging and null URL validation to the `generateUploadUrl` function.

--- app/api/file-handling/index.ts
+++ app/api/file-handling/index.ts
@@ -3,9 +3,14 @@ import axiosClient from "../axiosClient";
 export async function generateUploadUrl(
   fileName: string,
   fileType: string,
-  bucketName: string
+  bucketName: string,
 ): Promise<string | null> {
   try {
+    console.log("generateUploadUrl API call", {
+      fileName,
+      fileType,
+      bucketName,
+    });
     const response = await axiosClient.get<{ uploadUrl: string | null }>(
       "/file-handling/generateUploadUrl",
       {
@@ -14,14 +19,18 @@ export async function generateUploadUrl(
           fileType,
           bucketName,
         },
-      }
+      },
     );
+    console.log("generateUploadUrl API response", response.data);
     const { uploadUrl } = response.data;
+    if (!uploadUrl) {
+      throw new Error("S3 Upload url is null");
+    }
     return uploadUrl;
   } catch (error) {
     console.error(
       "There was a problem with the generateUploadUrl API call:",
-      error
+      error,
     );
     throw error;
   }
