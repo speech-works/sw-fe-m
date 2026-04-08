a1c0cad70ca84506127957817636201650c9ad0c
mayankav <mayankmacav@gmail.com>
2026-04-09 01:12:33 +0530

    chore: update company slogan in auth constants

--- app/screens/Auth/constants.ts
+++ app/screens/Auth/constants.ts
@@ -1,4 +1,4 @@
 export const COMPANY_NAME = "Speechworks";
-export const COMPANY_SLOGAN = "Practice. Connect. Improve.";
+export const COMPANY_SLOGAN = "Join the Movement. Change the Narrative.";
 export const PRIVACY_POLICY_URL = "https://www.speechworks.com/privacy-policy";
 export const SUPPORT_URL = "https://wa.me/971525609713";
